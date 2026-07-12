1c3a29cd8565de38c57ccb7c40be9ee2a89b2621
BigBirdReturns <bigbirdreturns@proton.me>
2026-07-12 12:49:45 -0700

    Export WOLF Ops playbooks

---
+++ src/ops/playbooks/index.ts
@@ -0,0 +1,5 @@
+export {
+  recessedLightingPlaybook,
+  recessedLightingDecisionCase,
+} from './recessedLighting.js';
+export { cafeDisplayPlaybook, cafeDisplayDecisionCase } from './cafeDisplay.js';

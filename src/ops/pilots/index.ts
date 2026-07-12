04947a740bd2dfbee1d5287a32ed66bf0127037f
BigBirdReturns <bigbirdreturns@proton.me>
2026-07-12 13:50:47 -0700

    Export WOLF Ops pilot templates

---
+++ src/ops/pilots/index.ts
@@ -0,0 +1,9 @@
+export {
+  buildRecessedLightingDecisionCase,
+  recessedLightingInspectionTemplate,
+} from './small-property-recessed-lighting.js';
+export type { RecessedLightingDecisionInput } from './small-property-recessed-lighting.js';
+export {
+  buildDisplayVentilationProcedure,
+  cafeDisplayInspectionTemplate,
+} from './small-cafe-display.js';
